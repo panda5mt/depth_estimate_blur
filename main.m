1337762ab1f173efec4e46208d5a83523dde02c0
Atsushi Saitoh <lynxeyed@live.jp>
2023-03-24 12:54:33 +0900

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+% 静止画の高速簡易深度推定(屋内)
 clear all;
 clc;
 img = imread('./img/WIN_20230316_17_12_59_Pro.jpg');
@@ -10,7 +11,7 @@ ref_V = rgb2hsv(img);
 ref_V = ref_V(:,:,3); 
 ref_scale = ref_V;
 
-% 3値化する(OTSU)
+% 3値化する(OTSU) 
 ref_gray_bk = ref_V; 
 gthresh1 = my_graythresh(ref_V);
 ref_V(ref_V > gthresh1) = 0; 
@@ -32,19 +33,20 @@ title("Otsu's method (3-values)")
 ref_SD = (abs(f_blur(img,4) - (img)));
 ref_SD = ref_SD(:,:,2);
 
-
-
 % depth estimation
+% 大津の手法によりセグメントした結果に疎な深度推定を反映させ、
+% 密(dense)な結果にする
+% 言い換えると、大津の3値化により簡易的に物体検出をし、
+% defocus blurによる結果で塗り絵をしているだけ
 im_width = width(ref_SD);
 im_height = height(ref_SD);
 
 img_FD = zeros(size(ref_SD));
 fill_enable = false;
 edge_factor = 0;
-
 N = 10; % フィルタ演算する1辺の長さ = N x N (pixel)
 
-for i=1:N:im_width-N % i=im_width-N:-N:6
+for i=1:N:im_width-N 
     for j=1:N:im_height-N 
         pick_defocus =  ref_SD(j:j+N-1,i:i+N-1);
         pick_matrices = ref_V(j:j+N-1,i:i+N-1);
