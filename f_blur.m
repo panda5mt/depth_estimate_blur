a3f3a74969399a8b094b25f05c9728ce59de6754
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-07 10:55:59 +0900

    Update f_blur.m

--- f_blur.m
+++ f_blur.m
@@ -15,7 +15,7 @@ function img = f_blur(img, n)
             i(y,:) = conv(i(y,:),kernel_1d,'same');
         end
         for x=1:x_size
-            i(:,x) = medfilt1(double(i(:,x)),floor(n/3));
+            i(:,x) = medfilt1(double(i(:,x)),floor(n/2));
             %i(:,x) = conv(i(:,x),kernel_1d,'same');
         end
         img(:,:,c) = i;
