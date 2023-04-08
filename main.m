63e0632bb891f3f9931bea65ecb5f7809792c2b3
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-08 14:34:28 +0900

    fixed blur bug

--- main.m
+++ main.m
@@ -36,10 +36,10 @@ ref_val(ref_gray_bk >= gthresh1) = 0.0;
 tick = tic;
 
 %% sparse defocus blur
-ref_spa = (img - (f_blur(img,8)));
+ref_spa = ((f_blur(img,4)) - (f_blur(img,16)));
 ref_spa = ref_spa(:,:,2);
-e = edge(im2gray(img),'log'); % あとで手実装する    
-ref_spa(e == 0) = 0;
+% e = edge(im2gray(img),'log'); % あとで手実装する    
+% ref_spa(e == 0) = 0;
 
 % 輪郭を確認する場合は下記3行をコメントアウト解除
 % figure(2)
