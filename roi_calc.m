4e260788c179eed37a90ef9b31bf06d6b06a1a4f
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-13 12:29:31 +0900

    Update roi_calc.m

--- roi_calc.m
+++ roi_calc.m
@@ -1,8 +1,8 @@
 function [Y,Z] = roi_calc(imgCrop)
     
     B = f_blur(imgCrop, 3, 2) - f_blur(imgCrop, 15, 2);
-    e = edge(im2gray(imgCrop),"log");
+    e = edge(im2gray(imgCrop),'log');
     B(e==0)=0;
-    Y = mean(B,"all")^2/2;
-    Z = var(double(B),0,"all");
+    Y = mean(B,'all')^2/2;
+    Z = var(double(B),0,'all');
 end
