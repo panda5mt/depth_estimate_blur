a98d15928c445bce14729a75897b7eddd0f68883
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-16 13:16:02 +0900

    Update roi_depth_main.m

--- roi_depth_main.m
+++ roi_depth_main.m
@@ -1,22 +1,26 @@
 % 関心領域を深度推定
 clear; close; clc;
-
 check_blur = false;
 
-vid_read = VideoReader('./img/driverec.mp4','CurrentTime',640);
-if hasFrame(vid_read)
-    
-img = readFrame(vid_read);
-img = imresize(img, [480 640]);
+vid_read = VideoReader('./img/driverec.mp4','CurrentTime',649);
 
+roi1 = [316.0000  189.0000  106.0000  108.0000];
+%roi1 = [];
+% 一旦1フレーム目の画像を出しておく
+if hasFrame(vid_read)    
+    img = readFrame(vid_read);
+    img = imresize(img, [480 640]);
 end
 
 figure(1)
 imshow(img)
 
-rect = drawrectangle('Label','distance-estimate','Color',[1 0 0]);
-roi1 = rect.Position; % 囲った部分を記憶
-roi1
+% roiの指定がなければ手動指定
+if isempty(roi1)
+    rect = drawrectangle('Label','distance-estimate','Color',[1 0 0]);
+    roi1 = rect.Position; % 囲った部分を記憶
+end
+
 while hasFrame(vid_read)
     img = readFrame(vid_read);
     img = imresize(img, [480 640]);
