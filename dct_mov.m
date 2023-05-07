b0ab7fc1f4cedc3d58ff9e95bb5f47a432764c08
Atsushi Saitoh <lynxeyed@live.jp>
2023-05-07 23:34:56 +0900

    Update dct_mov.m

--- dct_mov.m
+++ dct_mov.m
@@ -1,8 +1,9 @@
-%% 弱いストラクチャ部分を検出し、強いストラクチャからの深度情報を反映させるプログラム
-
+%% 弱いストラクチャ部分を検出し、強いストラクチャからの深度情報を反映させるプログラム(動画) 
+% inspired paper:"Blind Photograph Watermarking with Robust Defocus-Based JND Model"
+% https://www.hindawi.com/journals/wcmc/2020/8892349/
 clear;
 clc; 
-out_mov = false;
+out_mov = true;
 
 vid_read = VideoReader('./img/driverec.mp4','CurrentTime',650);
 if out_mov
@@ -35,7 +36,6 @@ end
     ETM = []; % Matrix of ET
     ELM = []; % Matrix of EL
     EMM = []; % Matrix of EM
-    ACM = []; % Matrix of AC Coeff
     
     for xx=1:8:size(B2,2)
         for yy=1:8:size(B2,1)
@@ -86,54 +86,42 @@ end
     S_ELM(~STB) = 0;
     S_EMM(~STB) = 0;
     
-    S_ELM = (S_ELM < (C0 .* C.^0.40)) .* S_ELM; 
-    S_EMM = (S_EMM < (C0 .* C.^0.25)) .* S_EMM; 
+    %S_ELM = (S_ELM < (C0 .* C.^0.40)) .* S_ELM; 
+    %S_EMM = (S_EMM < (C0 .* C.^0.25)) .* S_EMM; 
     EE = (S_ELM + S_EMM) ;  % Equation (14)
-    
-    
+    EE = S_ELM;
     %% Calc DM on WTB
     ELM(~WTB) = 0;
     EMM(~WTB) = 0;
-     ds = -0.003;
-    % Equation(15)
-    S = (log10(abs(ELM)) - log10(abs(EMM))) / ((log10(0.65)-log10(1.))); % TODO: fix S
-    %S = (log10(abs(ELM)) - log10(abs(EMM))) / ds; % TODO: fix S
     
-    S = (abs(S) > 1) .* S .* 1.4  + (abs(S) <= 1) .* 0; 
-
+    % Equation(15)
+    S = (log10(abs(ELM)) - log10(abs(EMM))) / ((log10(0.85)-log10(1.))); % TODO: fix S
+    %S = (abs(S) > 1) .* S .* 1.4  + (abs(S) <= 1) .* S .* 0; 
+    
     % % ↓弱いストラクチャの認識
     %S(abs(S) < 10) = 0;
     
+    % 合成
     %DM = (EE > 0) .* EE + (EE <= 0).* S;
-    DM = medfilt2(S,[8 8]);
+    DM  = S;
+    
+    % Osafune Blur
+    DMAP = f_blur(DM,10,1); % if size(image) <= (480,640), n = 8 or less. 
+    DMAP = log10(abs(DMAP));
 
     % 表示
-    figure(4);
+    figure(1);
+    cl = [0 3.5]; % clim value
+    tiledlayout(1,2)
+    nexttile
+    imagesc(EE);colormap(jet);clim(cl);
+    nexttile
+    imagesc(DMAP);colormap(jet);clim(cl);colorbar;
+    drawnow;
+
     if out_mov
-        im = ind2rgb(uint8(log10(abs(DM))), jet(2));
-        writeVideo(vid_write,im); 
-        imshow(im)
-    else
-        cl = [0 3]; % clim
-        tiledlayout(1,2)
-        nexttile
-        imagesc(EE);colormap(jet);clim(cl);
-        nexttile
-        imagesc(log10(abs(DM)));colormap(jet);clim(cl);colorbar;
-        drawnow;
-    
-
-    
-    %DM = get_dfm(img,DM,5);
-    %figure(1);imagesc(SX);colorbar;
-    %figure(2);imagesc(S);clim([0 2.5]);colorbar;
-    %figure(3);imagesc(DMX);colorbar;
-    % invdct = @(block_struct) T' * block_struct.data * T;
-    % K = blockproc(B2,[8 8],invdct);
-    
-    %figure(3);colormap('jet');imagesc(K);colorbar;clim([0 .5])
-
-
+        writeVideo(vid_write,getframe(gcf)); 
+    end
    
     % dct = @(block_struct) T * block_struct.data * T';
     % B = blockproc(img,[8 8],dct);
@@ -144,8 +132,6 @@ end
     % im = ind2rgb(255-(im2uint8(img) - im2uint8(K)));
     % figure(3);imshow(im);
     % drawnow
-    
-    end
 end
 
 if out_mov, close(vid_write); end
